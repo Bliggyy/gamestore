7f6f8ac1c522ab3b94b3914b08bdb315b2fb5fcc
Bligh Ignacio <73883691+Bliggyy@users.noreply.github.com>
2026-02-24 17:47:38 +0800

    feat: Added simple fetching of games on a fixed list

--- GameStore/Program.cs
+++ GameStore/Program.cs
@@ -1,8 +1,27 @@
+using GameStore.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+List<GameDto> games =
+[
+    new GameDto(
+        1,
+        "The Legend of Zelda: Breath of the Wild",
+        "Action-adventure",
+        59.99m,
+        new DateOnly(2017, 3, 3)
+    ),
+    new GameDto(2, "Super Mario Odyssey", "Platformer", 59.99m, new DateOnly(2017, 10, 27)),
+    new GameDto(3, "Red Dead Redemption 2", "Action-adventure", 59.99m, new DateOnly(2018, 10, 26)),
+    new GameDto(4, "The Witcher 3: Wild Hunt", "Action RPG", 39.99m, new DateOnly(2015, 5, 19)),
+    new GameDto(5, "Minecraft", "Sandbox", 26.95m, new DateOnly(2011, 11, 18)),
+];
 
+// GET games
+app.MapGet("/games", () => games);
 
-app.MapGet("/", () => "Hello World!");
+// GET game by id /games/{id}
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id));
 
 app.Run();
