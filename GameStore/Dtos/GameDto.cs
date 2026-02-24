79138f27a99fecf0ec091fd8fb16c799702e4137
Bligh Ignacio <73883691+Bliggyy@users.noreply.github.com>
2026-02-24 17:29:00 +0800

    chore: Added meaning of DTO

--- GameStore/Dtos/GameDto.cs
+++ GameStore/Dtos/GameDto.cs
@@ -1,7 +1,10 @@
 namespace GameStore.Dtos;
 
+// DTO is used to transfer data between layers of the application, such as from the 
+// database to the client. It can contain properties that represent the data we want to expose 
+// to the client, and it can also include validation attributes to ensure that the data is valid 
+// before it is sent to the client.
 public record class GameDto
 {
     
-
 }
