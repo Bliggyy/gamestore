b7d1b6b3afe958ea826b74c1e74d782a91ca0290
Bligh Ignacio <73883691+Bliggyy@users.noreply.github.com>
2026-02-25 22:36:52 +0800

    feat-fix: Added game store db setup within another file and fixed git tracking on obj and bin folder

--- GameStore/Data/DataExtensions.cs
+++ GameStore/Data/DataExtensions.cs
@@ -1,3 +1,4 @@
+using GameStore.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Data;
@@ -9,5 +10,39 @@ public static class DataExtensions
         using var scope = app.Services.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
         dbContext.Database.Migrate();
+    }
+
+    public static void AddGameStoreDb(this WebApplicationBuilder builder)
+    {
+        var connString = builder.Configuration.GetConnectionString("GameStore");
+
+        builder.Services.AddSqlite<GameStoreContext>(
+            connString,
+            optionsAction: options =>
+            {
+                options.UseSeeding(
+                    (context, _) =>
+                    {
+                        if (!context.Set<Genre>().Any())
+                        {
+                            context
+                                .Set<Genre>()
+                                .AddRange(
+                                    new Genre { Name = "Action" },
+                                    new Genre { Name = "Adventure" },
+                                    new Genre { Name = "RPG" },
+                                    new Genre { Name = "Strategy" },
+                                    new Genre { Name = "Sports" },
+                                    new Genre { Name = "Action-adventure" },
+                                    new Genre { Name = "Action RPG" },
+                                    new Genre { Name = "Sandbox" }
+                                );
+
+                            context.SaveChanges();
+                        }
+                    }
+                );
+            }
+        );
     }
 }
