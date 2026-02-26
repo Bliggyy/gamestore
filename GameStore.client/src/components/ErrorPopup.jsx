a8e608bacf13154cd4dec19eed6cf4765bb577a4
Bligh Ignacio <73883691+Bliggyy@users.noreply.github.com>
2026-02-26 17:47:46 +0800

    refactor: Made error box not dismissable and made it more simple

--- GameStore.client/src/components/ErrorPopup.jsx
+++ GameStore.client/src/components/ErrorPopup.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react';
-import { Alert } from 'react-bootstrap';
+import { Alert } from "react-bootstrap";
 
 export default function ErrorPopup({ message }) {
-    const [show, setShow] = useState(true);
-
-    if (!show) return null;
-
-    return (
-        <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-            <Alert.Heading>Error</Alert.Heading>
-            <p>{message}</p>
-        </Alert>
-    );
+  return (
+    <Alert variant="danger">
+      <Alert.Heading>Error</Alert.Heading>
+      <p>{message}</p>
+    </Alert>
+  );
 }
