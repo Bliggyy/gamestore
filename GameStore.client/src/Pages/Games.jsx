f3b310027fb97942a5c337e7b30019529e7f8f0c
Bligh Ignacio <73883691+Bliggyy@users.noreply.github.com>
2026-02-26 20:13:58 +0800

    chore: Removed unecessary constant games list

--- GameStore.client/src/Pages/Games.jsx
+++ GameStore.client/src/Pages/Games.jsx
@@ -4,45 +4,6 @@ import { fetchGames } from "../api/game";
 import Loading from "../components/Loading";
 import GameCard from "../components/GameCard";
 
-const gamesList = [
-  {
-    id: 1,
-    title: "The Legend of Zelda: Breath of the Wild",
-    genre: "Adventure",
-    description: "An open-world adventure game set in the kingdom of Hyrule.",
-    price: 59.99,
-  },
-  {
-    id: 2,
-    title: "God of War",
-    genre: "Action",
-    description:
-      "A mythological action game following Kratos and his son Atreus.",
-    price: 49.99,
-  },
-  {
-    id: 3,
-    title: "Red Dead Redemption 2",
-    genre: "Action-Adventure",
-    description: "An epic tale of life in America’s unforgiving heartland.",
-    price: 39.99,
-  },
-  {
-    id: 4,
-    title: "Minecraft",
-    genre: "Sandbox",
-    description: "A game about placing blocks and going on adventures.",
-    price: 26.95,
-  },
-  {
-    id: 5,
-    title: "Among Us",
-    genre: "Party",
-    description: "A multiplayer game of teamwork and betrayal.",
-    price: 4.99,
-  },
-];
-
 export default function Games() {
   const [games, setGames] = useState([]);
   const [loading, setLoading] = useState(true);
