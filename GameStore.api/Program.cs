7268d0a9d26f7180156719a1e584059aed9b4e62
Bligh Ignacio <73883691+Bliggyy@users.noreply.github.com>
2026-03-03 17:05:22 +0800

    fix: Fixed JWT service

--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -1,7 +1,29 @@
 using GameStore.Data;
 using GameStore.Endpoints;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder
+    .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters =
+            new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["JwtConfig:Issuer"],
+                ValidAudience = builder.Configuration["JwtConfig:Audience"],
+                IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(
+                    System.Text.Encoding.UTF8.GetBytes(
+                        builder.Configuration["JwtConfig:SecretKey"]!
+                    )
+                ),
+            };
+    });
 
 var corsSection = builder.Configuration.GetSection("CorsConfig");
 var corsPolicyName = corsSection.GetValue<string>("PolicyName")!;
@@ -19,6 +41,7 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddValidation();
+builder.Services.AddAuthorization();
 builder.AddGameStoreDb();
 
 var app = builder.Build();
