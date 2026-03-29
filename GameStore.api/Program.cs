cdc6da530bc350a7789a6c52788c7b910fc2b350
Bligh Ignacio <73883691+Bliggyy@users.noreply.github.com>
2026-03-29 13:07:09 +0800

    refactor: Moved authentication to its own AuthService file

--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -1,53 +1,10 @@
 using GameStore.Data;
 using GameStore.Endpoints;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using GameStore.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder
-    .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters =
-            new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = builder.Configuration["JwtConfig:Issuer"],
-                ValidAudience = builder.Configuration["JwtConfig:Audience"],
-                IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(
-                    System.Text.Encoding.UTF8.GetBytes(
-                        builder.Configuration["JwtConfig:SecretKey"]!
-                    )
-                ),
-            };
-
-        options.Events = new JwtBearerEvents
-        {
-            OnChallenge = context =>
-            {
-                context.HandleResponse();
-
-                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                context.Response.ContentType = "application/json";
-
-                var message = "The provided authentication token is invalid or missing.";
-
-                return context.Response.WriteAsync(message);
-            },
-            OnForbidden = context =>
-            {
-                context.Response.StatusCode = StatusCodes.Status403Forbidden;
-                context.Response.ContentType = "application/json";
-
-                var message = "You do not have permission to access this resource.";
-
-                return context.Response.WriteAsync(message);
-            },
-        };
-    });
+builder.SetAuthentication();
 
 var corsSection = builder.Configuration.GetSection("CorsConfig");
 var corsPolicyName = corsSection.GetValue<string>("PolicyName")!;
