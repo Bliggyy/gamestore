28e6e6a9119cf1cc0983ebf191e9844e5fd825b7
Bligh Ignacio <73883691+Bliggyy@users.noreply.github.com>
2026-03-30 20:20:38 +0800

    chore: Fixed layout of condition when using useCart function

--- GameStore.client/src/context/CartContext.jsx
+++ GameStore.client/src/context/CartContext.jsx
@@ -47,6 +47,8 @@ export const CartProvider = ({ children }) => {
 
 export const useCart = () => {
   const context = useContext(CartContext);
-  if (!context) throw new Error("useCart must be used within a CartProvider");
+  if (!context) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
   return context;
 };
